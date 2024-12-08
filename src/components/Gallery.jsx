a3dfcea66b66be193a7ec30955c2e8ea5209434a
Denys Zaretskyi <denis.zaretskyi@icloud.com>
2024-12-08 16:57:15 +0200

    Small changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,19 @@
 import { getPhotos } from "../QueryCustomHooks";
 import { useGlobalContext } from "../Context";
 import blackSpinner from "../assets/infinite-spinner-black.svg";
+import whiteSpinner from "../assets/infinite-spinner-white.svg";
 const Gallery = () => {
   const { queryVal } = useGlobalContext();
-  const { data, isLoading, isError } = getPhotos(queryVal);
+  const { data, isLoading, isError, error } = getPhotos(queryVal);
   const { darkTheme } = useGlobalContext();
   const photos = data?.data?.results;
-
-  if (isLoading) return <img id="LoadingIcon" src={blackSpinner} />;
-  if (isError) return <h1 id="ErrorMessage">Error...</h1>;
+  const getSpinner = !darkTheme ? (
+    <img id="LoadingIcon" src={blackSpinner} />
+  ) : (
+    <img id="LoadingIcon" src={whiteSpinner} />
+  );
+  if (isLoading) return getSpinner;
+  if (isError) return <h1 id="ErrorMessage">Error</h1>;
 
   return (
     <div className="gallery">
